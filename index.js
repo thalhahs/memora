fda648b04c0732208bdbb928d5dfdb10a6a465aa
thalhahs <thalhah@gmail.com>
2025-10-04 16:22:08 +0700

    Adding contacts to address book

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
-console.log("Address Book");
+console.log("Address Book (Batch 10)");
+
+const contacts = [
+    {
+        id: 1,
+        name: "Ahmad Santoso",
+        email: "ahmad.santoso@gmail.com",
+        phone: "081234567890",
+        address: "Jl. Merdeka No. 10, Bandung, Jawa Barat"
+    },
+    {
+        id: 2,
+        name: "Siti Rahmawati",
+        email: "siti.rahmawati@gmail.com",
+        phone: "082145678901",
+        address: "Jl. Melati No. 22, Surabaya, Jawa Timur"
+    },
+    {
+        id: 3,
+        name: "Budi Pratama",
+        email: "budi.pratama@gmail.com",
+        phone: "085612345678",
+        address: "Jl. Pahlawan No. 5, Yogyakarta, DIY"
+    },
+    {
+        id: 4,
+        name: "Dewi Lestari",
+        email: "dewi.lestari@gmail.com",
+        phone: "087812345612",
+        address: "Jl. Sudirman No. 45, Jakarta Selatan, DKI Jakarta"
+    },
+    {
+        id: 5,
+        name: "Rizky Hidayat",
+        email: "rizky.hidayat@gmail.com",
+        phone: "089512345632",
+        address: "Jl. Diponegoro No. 8, Medan, Sumatera Utara"
+    },
+    {
+        id: 6,
+        name: "Nur Aisyah",
+        email: "nur.aisyah@gmail.com",
+        phone: "081356789012",
+        address: "Jl. Cendana No. 3, Makassar, Sulawesi Selatan"
+    },
+    {
+        id: 7,
+        name: "Fajar Maulana",
+        email: "fajar.maulana@gmail.com",
+        phone: "082345678921",
+        address: "Jl. Gatot Subroto No. 14, Denpasar, Bali"
+    },
+    {
+        id: 8,
+        name: "Lestari Anindya",
+        email: "lestari.anindya@gmail.com",
+        phone: "085234567899",
+        address: "Jl. Kartini No. 9, Semarang, Jawa Tengah"
+    },
+    {
+        id: 9,
+        name: "Andi Wijaya",
+        email: "andi.wijaya@gmail.com",
+        phone: "087745612345",
+        address: "Jl. Ahmad Yani No. 20, Palembang, Sumatera Selatan"
+    },
+    {
+        id: 10,
+        name: "Maya Putri",
+        email: "maya.putri@gmail.com",
+        phone: "089912345678",
+        address: "Jl. Gajah Mada No. 15, Balikpapan, Kalimantan Timur"
+    }
+];
+
+console.log(contacts);
